392a45d040b9595e32fdc31ac15f887c9687941d
mojtabahadei81 <m.hadei.1122@gmail.com>
2026-06-25 03:42:54 -0700

    feat: add .env file and use os.environ to get url from env

--- step2_api.py
+++ step2_api.py
@@ -1,7 +1,11 @@
+import os
 import requests
 from pydantic import BaseModel, EmailStr, ValidationError
 import json
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logging.basicConfig(
     level='INFO',
@@ -10,29 +14,35 @@ logging.basicConfig(
 
 logger = logging.getLogger()
 
-url = 'https://jsonplaceholder.typicode.com/users'
-
 class User(BaseModel):
     id: int
     name: str
     email: EmailStr
 
-def get_user() -> None:
+def get_user(url:str) -> None:
+    verificated_users = []
     try:
         response = requests.get(url)
         response.raise_for_status()
-        data = response.json()
-        
-        User(data['id'], data['name'], data['email'])
+        users = response.json()
+        for user in users:
+            try:
+                data = User(**user)
+                verificated_users.append(data.model_dump())
+            except ValidationError as val_err:
+                logger.error(f"Validation error occurred: {val_err}")
 
         with open('users.json', 'w') as f:
-            json.dump(data, f)
+            json.dump(verificated_users, f, indent=4)
 
     except requests.exceptions.HTTPError as http_err:
         logger.error(f"HTTP error occurred: {http_err}")
     except requests.exceptions.RequestException as err:
         logger.error(f"Other error occurred: {err}")
-    except ValidationError as val_err:
-        logger.error(f"Validation error occurred: {err}")
 
-get_user()
+try:
+    url = os.environ['API_URL']
+except KeyError as e:
+    logger.error(f'ERROR: key error occured: {e}')
+else:    
+    get_user(url)
